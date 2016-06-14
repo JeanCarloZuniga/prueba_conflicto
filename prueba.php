8dc42633d166171c26019b5001f8fde6e49ec301
Jean Carlo <jeanczm@gmail.com>
2016-06-14 17:41:36 +0000

    DUIS

--- prueba.php
+++ prueba.php
@@ -1,5 +1,7 @@
 <?php
     echo "hola fuck you jean";
-    print_r ("Que trama");
+    print_r ("NO FOCARRON");
     echo "Que renta este hpta";
+    echo "rancho de pero";
+    echo "Me solidifico en Belzebub";
 ?>
